5299a5db64db694f9cdcff789647f5379380f02a
Alexandre DAUSSIN <42854044+AlexandreDAUSSIN@users.noreply.github.com>
2023-12-22 13:59:47 +0100

    correction

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ async function main() {
     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
     standardHeaders: true, // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    validate: {xForwardedForHeader: false}
   })
 
   //Définir l'application
